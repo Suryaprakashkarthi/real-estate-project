679dc7b6781b797b743e7283f69cde803f8f21b5
Surya <125908373+Suryaprakashkarthi@users.noreply.github.com>
2025-12-15 00:46:19 +0530

    Wrap routes in BrowserRouter for routing support

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,19 @@
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import PropertyList from "./PropertyList";
 import Book from "./Book";
 import Admin from "./Admin";
 import AdminBookings from "./AdminBookings";
+
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<PropertyList />} />
-      <Route path="/book/:id" element={<Book />} />
-      <Route path="/admin" element={<Admin />} />
-      <Route path="/admin/bookings" element={<AdminBookings />} />
-    </Routes>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<PropertyList />} />
+        <Route path="/book/:id" element={<Book />} />
+        <Route path="/admin" element={<Admin />} />
+        <Route path="/admin/bookings" element={<AdminBookings />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
